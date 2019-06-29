8168445d398b5c7462a807d966835e7c1bd91a5b
Robert Rice <h4110w33n@gmail.com>
2019-06-29 16:10:13 -0400

    Updating the Makefile and giving everything a version bump.

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2019, Robert Rice'
 author = 'Robert Rice'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 
